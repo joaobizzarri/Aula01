82b8a371794cfbb00e71361ae20a5178c8ae5e43
Joao Bizzarri <joaobizzarri@gmail.com>
2021-06-23 19:58:34 -0300

    Versão final da aula 1

---
+++ src/LendoDados.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class LendoDados {
+
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+		String nome;
+		
+		System.out.println("Por favor, insisra seu nome");
+		nome = leitor.nextLine();
+		System.out.println("Bem vindo, " + nome);
+		leitor.close();
+	}
+
+}
